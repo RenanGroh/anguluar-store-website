4c58602d9bd05e82394b8b9f7fe91f548999c378
RenanGroh <renangroh@gmail.com>
2025-04-23 13:42:44 -0300

    build para GitHub Pages

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-A7U3UEM7.mjs";import"./chunk-LCPVAAML.mjs";import"./chunk-DEYCEND2.mjs";import"./chunk-TNUDU6IU.mjs";import"./chunk-GTSSCL6B.mjs";import"./chunk-5HRW67DO.mjs";import"./chunk-SDSMD3SL.mjs";import"./chunk-ALR33533.mjs";import"./chunk-6V54WZTC.mjs";import"./chunk-JA2XQ5JD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
